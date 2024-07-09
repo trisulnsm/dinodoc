6522b85aa05102bfd19511b239c8229d56d6fb9f
vignesh-1064 <vigneshd@unleashnetworks.com>
2024-07-09 17:37:06 +0530

    removed tot.dat

--- trisuldocs/docs/lua/controller.rb
+++
@@ -1,6 +0,0 @@
-before /textile/  do
-  @activetab = {:devs => 'active'}
- 	@toc =  extract_toc(@_stasis.path)
- 	@newcontent = decorate(@_stasis.path)
-	layout '/layouts/lua/layout_sidebar_boxed.html.erb'
-end
