774523accad45152345d39cdc614ae1c135023b5
vignesh-1064 <vigneshd@unleashnetworks.com>
2024-07-17 18:42:17 +0530

    added search-box and edited docusarus.config file

--- trisuldocs/docusaurus.config.js
+++ trisuldocs/docusaurus.config.js
@@ -53,7 +53,14 @@ const config = {
       }),
     ],
   ],
-
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+      },
+    ],
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
